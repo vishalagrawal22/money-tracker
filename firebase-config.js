631adaf312fcfbb179be1408dac17a7712934ec6
Vishal Agrawal <vishalagrawalva22@gmail.com>
2023-01-06 21:45:05 +0530

    Update firebase config

--- firebase-config.js
+++ firebase-config.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCtnzHoasYcJysnX96JNgE_uTaOP0lLgjs",
-  authDomain: "task-scheduler-b7f9e.firebaseapp.com",
-  projectId: "task-scheduler-b7f9e",
-  storageBucket: "task-scheduler-b7f9e.appspot.com",
-  messagingSenderId: "963971397523",
-  appId: "1:963971397523:web:23bf4338301f199b621a66",
+  apiKey: "AIzaSyAwjt_sJXtxDmMdc9ep04ypZylNbia9nbE",
+  authDomain: "money-tracker-e68a7.firebaseapp.com",
+  projectId: "money-tracker-e68a7",
+  storageBucket: "money-tracker-e68a7.appspot.com",
+  messagingSenderId: "1016553994686",
+  appId: "1:1016553994686:web:4538484b01f6dc2402a1ef",
 };
 
 const app = initializeApp(firebaseConfig);
