5883de52eae1346e904c11af79ed615ac9319391
Vishal Agrawal <vishalagrawalva22@gmail.com>
2023-01-09 01:45:39 +0530

    Break service file into multiple env variables for deployment to vercel

--- firebase-config/server.js
+++ firebase-config/server.js
@@ -1,13 +1,26 @@
 import admin from "firebase-admin";
 
-const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY);
-
 const app =
   admin.apps.length && admin.apps[0] !== null
     ? admin.apps[0]
     : admin.initializeApp({
-        credential: admin.credential.cert(serviceAccount),
-        databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+        credential: admin.credential.cert({
+          type: "service_account",
+          project_id: process.env.FIREBASE_PROJECT_ID,
+          private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+          private_key: process.env.FIREBASE_PRIVATE_KEY
+            ? Buffer.from(process.env.FIREBASE_PRIVATE_KEY, "base64").toString(
+                "utf8"
+              )
+            : undefined,
+          client_email: process.env.FIREBASE_CLIENT_EMAIL,
+          client_id: process.env.FIREBASE_CLIENT_ID,
+          auth_uri: "https://accounts.google.com/o/oauth2/auth",
+          token_uri: "https://oauth2.googleapis.com/token",
+          auth_provider_x509_cert_url:
+            "https://www.googleapis.com/oauth2/v1/certs",
+          client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL,
+        }),
       });
 
 const auth = admin.auth(app);
