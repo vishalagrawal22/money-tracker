4c94fd53fba5492a9eac775d1230530957240e9b
Vishal Agrawal <vishalagrawalva22@gmail.com>
2023-01-08 23:30:40 +0530

    Remove service file for smooth deployment to vercel

--- firebase-config/server.js
+++ firebase-config/server.js
@@ -1,10 +1,13 @@
 import admin from "firebase-admin";
 
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY);
+
 const app =
   admin.apps.length && admin.apps[0] !== null
     ? admin.apps[0]
     : admin.initializeApp({
-        credential: admin.credential.applicationDefault(),
+        credential: admin.credential.cert(serviceAccount),
+        databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
       });
 
 const auth = admin.auth(app);
