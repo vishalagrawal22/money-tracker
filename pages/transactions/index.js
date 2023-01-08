aead3a6172676021685289cfb95870a8bb4ac740
Vishal Agrawal <vishalagrawalva22@gmail.com>
2023-01-08 22:14:33 +0530

    Add create transaction button to transactions page

--- pages/transactions/index.js
+++ pages/transactions/index.js
@@ -1,9 +1,17 @@
+import Link from "next/link";
+import { Button } from "react-bootstrap";
+
 import Layout from "../../components/Layout";
 import TransactionList from "../../components/TransactionList";
 
 export default function TransactionPage() {
   return (
     <Layout>
+      <Link href="/transactions/create">
+        <Button variant="primary" active>
+          Create Transaction
+        </Button>
+      </Link>
       <TransactionList />
     </Layout>
   );
