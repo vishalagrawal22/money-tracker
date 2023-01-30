b1fe881faa96e43533a4ff8bcf6c5188a5c31854
Vishal Agrawal <vishalagrawalva22@gmail.com>
2023-01-30 18:45:13 +0530

    Remove tailwind from login page

---
+++ components/OrDivider.js
@@ -0,0 +1,11 @@
+function OrDivider() {
+  return (
+    <div className="d-flex align-items-center my-4">
+      <hr className="flex-grow-1" />
+      <span className="px-3 fw-light">OR</span>
+      <hr className="flex-grow-1" />
+    </div>
+  );
+}
+
+export default OrDivider;
